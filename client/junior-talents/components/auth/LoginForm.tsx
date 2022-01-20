f1cac2d12e11777b2eef569e7243af9b154ea853
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-01-20 11:36:36 +0200

    Add auth handle submit

--- client/junior-talents/components/auth/LoginForm.tsx
+++ client/junior-talents/components/auth/LoginForm.tsx
@@ -1,20 +1,29 @@
+import { useState } from 'react'
+
 import RegularButton from '../common/Button'
 
 import styles from '../../styles/auth.module.css'
 
 const LoginForm  = () => {
+    const [username, setUsername] = useState<string | null>()
+    const [password, setPassword] = useState<string | null>()
+
+    const login = () => {
+        console.log(username, password)
+    }
+
     return (
         <div className={styles['auth-container']}>
             <form className={styles['auth-form']} action="login" method="post">
                 <div className={styles['inset']}>
                     <p>
-                        <input type="text" className={styles['input']} name="username" id="username" placeholder="Потребителско име" />
+                        <input onChange={(e) => setUsername(e.target.value)} required type="text" className={styles['input']} name="username" id="username" placeholder="Потребителско име" />
                     </p>
                     <p>
-                        <input type="password" className={styles['input']}  name="password" id="password" placeholder="Парола" />
+                        <input onChange={(e) => setPassword(e.target.value)} required type="password" className={styles['input']}  name="password" id="password" placeholder="Парола" />
                     </p>
                 </div>
-                <RegularButton title='Вход'/>
+                <RegularButton onSubmit={login.bind(this)} title='Вход'/>
             </form>
         </div>
     )
--- client/junior-talents/components/auth/RegisterForm.tsx
+++ client/junior-talents/components/auth/RegisterForm.tsx
@@ -1,20 +1,28 @@
+import { useState } from 'react'
 import RegularButton from '../common/Button'
 
 import styles from '../../styles/auth.module.css'
 
 const RegisterForm  = () => {
+    const [username, setUsername] = useState<string | null>()
+    const [password, setPassword] = useState<string | null>()
+
+    const register = () => {
+        console.log(username, password)
+    }
+
     return (
         <div className={styles['auth-container']}>
             <form className={styles['auth-form']} action="login" method="post">
                 <div className={styles['inset']}>
                     <p>
-                        <input type="text" className={styles['input']} name="username" id="username" placeholder="Потребителско име" />
+                        <input onChange={(e) => setUsername(e.target.value)} required type="text" className={styles['input']} name="username" id="username" placeholder="Потребителско име" />
                     </p>
                     <p>
-                        <input type="password" className={styles['input']}  name="password" id="password" placeholder="Парола" />
+                        <input onChange={(e) => setPassword(e.target.value)} required type="password" className={styles['input']}  name="password" id="password" placeholder="Парола" />
                     </p>
                 </div>
-                <RegularButton title='Регистрация'/>
+                <RegularButton onSubmit={register.bind(this)} title='Регистрация'/>
             </form>
         </div>
     )
